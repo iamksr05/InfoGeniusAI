d7975a54e17aed97a62c81c115715f8449a229d5
Karan Ram <Karanram730005@gmail.com>
2025-09-05 16:26:33 +0530

    updated manifest

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,8 +1,35 @@
+const CACHE_NAME = 'infogenius-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/icons/manifest-icon-192.maskable.png',
+  '/icons/manifest-icon-512.maskable.png'
+];
+
+// Install event: cache files
 self.addEventListener('install', (event) => {
-  console.log('Service Worker installing...');
-  self.skipWaiting(); // Activate immediately
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
 });
 
+// Activate event
 self.addEventListener('activate', (event) => {
-  console.log('Service Worker activating...');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+      );
+    })
+  );
+});
+
+// Fetch event: serve cached files first
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
 });
