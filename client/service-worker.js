91cf0e82bdfe60e140947e5e4c7d2167a5ad1ab2
Karan Ram <Karanram730005@gmail.com>
2025-09-05 16:06:37 +0530

    updated manifest

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,10 +1,8 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(function(registration) {
-        console.log('Service Worker registered with scope:', registration.scope);
-      })
-      .catch(function(error) {
-        console.log('Service Worker registration failed:', error);
-      });
-  }
-  
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installing...');
+  self.skipWaiting(); // Activate immediately
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activating...');
+});
